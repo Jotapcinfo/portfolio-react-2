a429f718aac2baf5ab513a5da50f9328d30105e7
Jotapcinfo <prowlerurbanhc@gmail.com>
2023-07-11 02:22:40 -0300

    end project start deploy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import { FaFacebookF } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { IoLogoTwitter } from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Fraseadotec</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#sobre">Sobre</a></li>
+        <li><a href="#experiência">Experiência</a></li>
+        <li><a href="#serviços">Serviços</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#depoimentos">Depoimentos</a></li>
+        <li><a href="#contato">Contato</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Fraseadotec</small>
+      </div>
+    </footer>
   )
 }
 
